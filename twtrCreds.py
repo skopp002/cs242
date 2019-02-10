0c121b42b0e8c0b89db2e48a5042444400fdf34e
NationHealing <47338906+NationHealing@users.noreply.github.com>
2019-02-10 13:25:40 -0800

    Add files via upload

---
+++ twtrCreds.py
@@ -0,0 +1,6 @@
+# YouTube Video: https://www.youtube.com/watch?v=wlnx-7cm4Gg
+# Variables that contains the user credentials to access Twitter API
+ACCESS_TOKEN = "2562816018-B1JelcDKDxA6nGJqdpDUtWOcgukxtxMp99IvNBU"
+ACCESS_TOKEN_SECRET = "EvC8ODGFlhbf9pQ5fx4QoNIQixYABWgOLsUQKzjIMAHZa"
+CONSUMER_KEY = "kSMoVz9TReLonWGXyjyb0CYjv"
+CONSUMER_SECRET = "AyFVi56PjVsyWP8pC3q651UyBxs4uIhNpmMYaJDRg50SmYnRpU"
